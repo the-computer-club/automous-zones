b1210cf6b0687fba8299bd9411fc8763d6e8f004
Simcra <Simcra@live.com>
2024-05-19 11:23:51 +0930

    Update flake.nix (#14)
    Reorganized entries by IP address, updated my own public key and IP

--- flake.nix
+++ flake.nix
@@ -39,21 +39,6 @@
           publicKey = "qXsTs+IsmHeq9+rulmG6M7XhVgu/3N/wEgEaHPuHciU=";
           ipv4 = [ "172.16.2.4/32" ];
         };
-        
-        sky = {
-          publicKey = "EF4+fPzrAMqasS8Z7auajOuHmcvqcHVGQNPwhPo0/U8=";
-          ipv4 = [ "172.16.2.42/32" ];
-        };
-
-        simcra = {
-          publicKey = "iKKn0hxQdScrXXkpmIC83Zgo0Y3GexCQ4+Qd/6NryUc=";
-          ipv4 = [ "172.16.2.64/32" ];
-        };
-
-        ov13 = {
-          publicKey = "S6yiCMatKlVX0WxyaWXTizasZPfQQ9oGM2pv82CtrgM=";
-          ipv4 = [ "172.16.2.27/32" ];
-        };
 
         quantum = {
           publicKey = "FuOo9TwnE8P/zDBorjGNtsyPImHsHPxA21ePNrElsD0=";
@@ -67,38 +52,53 @@
 
         goatware = {
           publicKey = "YO5w0tMvPt2lDdg9eD25wZsz9rWMTGbA/KuPYB6F1jQ=";
-          ipv4 = ["172.16.2.7/32"];
+          ipv4 = [ "172.16.2.7/32" ];
         };
 
         pager1 = {
           publicKey = "NEN3VPGm58JLQkjUwq8MOhC/d7ZEZXcIjBl84ENGBzg=";
-          ipv4 = ["172.16.2.8/32"];
+          ipv4 = [ "172.16.2.8/32" ];
         };
+
+        simcra = {
+          publicKey = "pq529jYwkJZZzdWB2fJ08A+41prV5gVsg3iE/kVN0GQ=";
+          ipv4 = [ "172.16.2.9/32" ];
+        }
 
         "0xc000000f" = {
           publicKey = "58qZ8KjLz80JPfSgsWU7iPpS6bjy94NONVoufLGxeX4=";
-          ipv4 = ["172.16.2.10/32"];
+          ipv4 = [ "172.16.2.10/32" ];
+        };
+
+        ov13 = {
+          publicKey = "S6yiCMatKlVX0WxyaWXTizasZPfQQ9oGM2pv82CtrgM=";
+          ipv4 = [ "172.16.2.27/32" ];
+        };
+        
+        sky = {
+          publicKey = "EF4+fPzrAMqasS8Z7auajOuHmcvqcHVGQNPwhPo0/U8=";
+          ipv4 = [ "172.16.2.42/32" ];
+        };
+
+        frogson = {
+          publicKey = "5j44nM6qmbJ2S8B24aA/H6UEPVXJFfxf8sTacMktMis=";
+          ipv4 = [ "172.16.2.69/32" ];
+        };
+
+        "jita.etherealryft.net" = {
+          publicKey = "Ngp854R/d126OVrO7nRv0yj8rdHRjGURa5LbD/UT8Xk=";
+          ipv4 = [ "172.16.2.71/32" ];
+          selfEndpoint = "72.55.233.131:56854";
+        };
+
+        comet = {
+          publicKey = "w90lfP16gY5debtjkfKVyKdL8mtlEXUmciNRTyTi7jw=";
+          ipv4 = [ "172.16.2.96/32" ];
         };
 
         Scott = {
           publicKey = "5W6KorrxAf3MWErAYlgSXcGUNEJfBwPTBlyEfRRfeXU=";
           ipv4 = [ "172.16.2.128/32" ];
-        };
-
-        frogson = {
-          publicKey = "5j44nM6qmbJ2S8B24aA/H6UEPVXJFfxf8sTacMktMis=";
-          ipv4 = ["172.16.2.69/32"];
-        };
-
-        comet = {
-          publicKey = "w90lfP16gY5debtjkfKVyKdL8mtlEXUmciNRTyTi7jw=";
-          ipv4 = ["172.16.2.96/32"];
-        };
- 
-        "jita.etherealryft.net" = {
-          publicKey = "Ngp854R/d126OVrO7nRv0yj8rdHRjGURa5LbD/UT8Xk=";
-          selfEndpoint = "72.55.233.131:56854";
-          ipv4 = ["172.16.2.71/32"];
         };
 
         merosity = {
