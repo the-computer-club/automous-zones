28a893d6b9e2a5f1a3a44b8f9e386bea47a6a199
SkettiSouls <146829563+SkettiSouls@users.noreply.github.com>
2024-07-30 03:30:46 -0500

    Fix typo in fluorine's ipv4 (#22)

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
 
         fluorine = {
           publicKey = "fCw+r4TKsxh36CdDSc6BTf0an9F2O8KQ189dYukpFHs=";
-          ipv4 = [ "172.15.2.14/32" ];
+          ipv4 = [ "172.16.2.14/32" ];
         };
 
         pager1 = {
